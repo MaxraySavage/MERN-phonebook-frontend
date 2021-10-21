90043d79b3665b18a805a5a4afecbb3779b68d2d
MaxraySavage <maxraysavage@gmail.com>
2021-10-21 15:34:36 -0400

    use relative url for person service

--- src/services/personService.js
+++ src/services/personService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = 'https://aqueous-ridge-34409.herokuapp.com/api/persons';
+const baseUrl = '/api/people';
 
 const getAll = () => {
     return axios.get(baseUrl).then(response => response.data)
